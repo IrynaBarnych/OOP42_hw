37ccc9c73462123ccbbe071fba969aaa39b07b1a
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-14 00:48:23 +0200

    Завдання 2 Показати кількість овочів та фруктів кожного кольору.

--- OOP42_hw.sql
+++ OOP42_hw.sql
@@ -1,9 +1,9 @@
 --Завдання 2
---Показати кількість овочів та фруктів заданого кольору.
+--Показати кількість овочів та фруктів кожного кольору.
+
 
 SELECT color, type, COUNT(*) AS item_count
 FROM VegetablesAndFruits
-WHERE color = 'червоний'
 GROUP BY color, type;
 
 
