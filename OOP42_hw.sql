7a0e94c7b3d92bb088faf9714bcd4a814130c1de
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-14 00:51:42 +0200

    Завдання 2 Показати колір мінімальної кількості овочів та фруктів

--- OOP42_hw.sql
+++ OOP42_hw.sql
@@ -1,11 +1,14 @@
 --Завдання 2
---Показати кількість овочів та фруктів кожного кольору.
+--Показати колір мінімальної кількості овочів та фруктів
 
-
-SELECT color, type, COUNT(*) AS item_count
-FROM VegetablesAndFruits
-GROUP BY color, type;
-
+SELECT color, MIN(item_count) AS min_count
+FROM (
+    SELECT color, type, COUNT(*) AS item_count
+    FROM VegetablesAndFruits
+    GROUP BY color, type
+) 
+AS counts
+GROUP BY color;
 
 
 
